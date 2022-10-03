7f2f8ffdcd5387b5b6beb1998e5a886106481121
adam mikler <adammikler10@gmail.com>
2022-10-03 15:24:11 -0700

    added display function to create new div elements

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const authorInput = form.elements['author'];
 const numberPagesInput = form.elements['pagenum'];
 const readInput = form.elements['read'];
 
+//Selectors for showcasing books
+const bookContainer = document.getElementById('book-container');
+
 //Array to store book objects
 let library = [];
 
@@ -18,9 +21,6 @@ function Book (read, numberPages, title, author) {
 }
 
 function addBookToLibrary(currentbook) {
-    
-    
-    
     //Sets the variables = to the values found within the form
     let title = titleInput.value;
     let numberPages = numberPagesInput.value;
@@ -32,17 +32,57 @@ function addBookToLibrary(currentbook) {
 
     //pushes new object to library array
     library.push(currentbook);
+
+    clearInputs();  //Clears text inputs
+    displayBook(); 
 };
 
+//clears the input in form
 function clearInputs() {
-    titleInput.value = "";
-    numberPagesInput.value = "";
-    authorInput.value = "";
-    readInput.value = "";
+    titleInput.value = '';
+    numberPagesInput.value = '';
+    authorInput.value = '';
+    readInput.value = '';
+};
+
+function displayBook() {
+    //adds a new div to contain book values
+    const bookDiv = document.createElement('div');  //creates the div
+    bookDiv.classList.add('book');  //div class book
+    bookDiv.setAttribute('id', 'bookDiv');  //div ID bookDiv
+
+    bookContainer.appendChild(bookDiv);  //adds the bookDiv to the book-container
+
+    const bookBox = document.getElementById('bookDiv'); //Selects the book div to add generate P elements within
+    //bookBox.classList.add("displayBook");   //adds displayBook class
+
+    const titleDisplay = document.createElement('p');
+    titleDisplay.classList.add('displaytitle')
+
+    const authorDisplay = document.createElement('p');
+    authorDisplay.classList.add('displayauthor');
+
+    const pageNumDisplay = document.createElement('p');
+    pageNumDisplay.classList.add('display-pagecount');
+
+    const readStatus = document.createElement('p');
+    readStatus.classList.add('display-read');
+
+    //adds the new P elements to the parent div
+    bookBox.appendChild(titleDisplay);
+    bookBox.appendChild(authorDisplay);
+    bookBox.appendChild(pageNumDisplay);
+    bookBox.appendChild(readStatus);
+
+    titleDisplay.innerHTML = "title";
+    authorDisplay.innerHTML = "author";
+    pageNumDisplay.innerHTML = "200";
+    readStatus.innerHTML = "Read"
+
 }
 
 //adds event listener for submit button
-const submit = document.getElementById("submit");
+const submit = document.getElementById('submit');
 submit.addEventListener('click', (event) => {
     event.preventDefault();    //prevents form from clearing before info is used
 
@@ -51,6 +91,9 @@ submit.addEventListener('click', (event) => {
     addBookToLibrary();  //creates new book on submit event
     currentbook += 1;   //incriments the current book to use for the ID of each book
 
-    clearInputs();
     console.log(library);
 });
+
+function Main() {
+
+};
